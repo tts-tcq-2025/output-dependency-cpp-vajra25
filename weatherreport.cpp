b3c5e6e6b6e1b547d3067e1edc3c1a9a9c2a8ff1
Vajra <VajraJagadish.Shetty@in.bosch.com>
2025-10-15 11:52:34 +0530

    Update weatherreport.cpp

--- weatherreport.cpp
+++ weatherreport.cpp
@@ -7,39 +7,24 @@ using namespace std;
 namespace WeatherSpace
 {    
     class IWeatherSensor {
-        public:
-            virtual double TemperatureInC() const = 0;
-            virtual int Precipitation() const = 0;
-            virtual int Humidity() const = 0;
-            virtual int WindSpeedKMPH() const = 0;
+    public:
+        virtual double TemperatureInC() const = 0;
+        virtual int Precipitation() const = 0;
+        virtual int Humidity() const = 0;
+        virtual int WindSpeedKMPH() const = 0;
     };
-    /// <summary>
-    /// This is a stub for a weather sensor. For the sake of testing 
-    /// we create a stub that generates weather data and allows us to
-    /// test the other parts of this application in isolation
-    /// without needing the actual Sensor during development
-    /// </summary>
+
+    // Original production stub (can stay, not used in tests)
     class SensorStub : public IWeatherSensor {
-        int Humidity() const override {
-            return 72;
-        }
-
-        int Precipitation() const override {
-            return 70;
-        }
-
-        double TemperatureInC() const override {
-            return 26;
-        }
-
-        int WindSpeedKMPH() const override {
-            return 52;
-        }
+        int Humidity() const override { return 72; }
+        int Precipitation() const override { return 70; }
+        double TemperatureInC() const override { return 26; }
+        int WindSpeedKMPH() const override { return 52; }
     };
+
     string Report(const IWeatherSensor& sensor)
     {
         int precipitation = sensor.Precipitation();
-        // precipitation < 20 is a sunny day
         string report = "Sunny Day";
 
         if (sensor.TemperatureInC() > 25)
@@ -51,23 +36,27 @@ namespace WeatherSpace
         }
         return report;
     }
-    
+
+    // New sensor stubs to expose bug
+    class RainySensorStub : public IWeatherSensor {
+    public:
+        double TemperatureInC() const override { return 28; }
+        int Precipitation() const override { return 80; } // high precipitation
+        int Humidity() const override { return 70; }
+        int WindSpeedKMPH() const override { return 30; } // low wind
+    };
+
     void TestRainy()
     {
-        SensorStub sensor;
+        RainySensorStub sensor;
         string report = Report(sensor);
         cout << report << endl;
-        assert(report.find("rain") != string::npos);
+        assert(report.find("rain") != string::npos); // will fail
     }
 
     void TestHighPrecipitation()
     {
-        // This instance of stub needs to be different-
-        // to give high precipitation (>60) and low wind-speed (<50)
         SensorStub sensor;
-
-        // strengthen the assert to expose the bug
-        // (function returns Sunny day, it should predict rain)
         string report = Report(sensor);
         assert(report.length() > 0);
     }
