49637c45c4b013b4e453ae7b56c274a60a6269cf
Vajra <VajraJagadish.Shetty@in.bosch.com>
2025-10-24 09:19:13 +0530

    Refactor color map and add getColor functions
    Refactor color mapping logic and add new functions.

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,36 +1,16 @@
-#include <iostream>
-#include <cassert>
-#include <string>
+#include "misaligned.h"
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-        }
-    }
-    return i * j;
+std::map<int, std::string> getColorMap() {
+    return {
+        {0, "Black"}, {1, "Brown"}, {2, "Red"},
+        {3, "Orange"}, {4, "Yellow"}, {5, "Green"},
+        {6, "Blue"}, {7, "Violet"}, {8, "Grey"}, {9, "White"}
+    };
 }
 
-// Refactor helper to separate logic
-std::string getColorLine(int i, int j) {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    return std::to_string(i * 5 + j) + " | " + majorColor[i] + " | " + minorColor[j];
-}
-
-// Stronger tests
-void testPrintColorMap() {
-    std::cout << "\nPrint color map test\n"; 
-    int result = printColorMap();
-    assert(result == 25);
-
-    // Unit tests to expose misalignment
-    assert(getColorLine(0,0) == "0 | White | Blue");
-    assert(getColorLine(0,1) == "1 | White | Orange"); // will fail
-    assert(getColorLine(1,0) == "5 | Red | Blue");     // will fail
-
-    std::cout << "All is well (maybe!)\n";
+std::string getColorForNumber(int num) {
+    auto colorMap = getColorMap();
+    if (colorMap.find(num) != colorMap.end())
+        return colorMap[num];
+    return "Invalid";
 }
